d0bfaed8471d4caf44a3f7103a15e3a8b59884ee
santiagobi1406-design <santiagobi1406@gmail.com>
2026-06-03 15:03:08 -0500

    initial commit

---
+++ app/Models/Progress.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Progress extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProgressFactory> */
+    use HasFactory;
+}
---
+++ database/factories/ProgressFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Progress;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Progress>
+ */
+class ProgressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
